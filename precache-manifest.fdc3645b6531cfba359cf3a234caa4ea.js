dc762132e4f3a78272f135a331679b67be5efb4e
KayinCheung <cheungkayin00@gmail.com>
2019-05-13 01:11:15 +0800

    Initial commit
    Testing order submission api

---
+++ precache-manifest.fdc3645b6531cfba359cf3a234caa4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebba8b1cdbb6cad1442d",
+    "url": "/static/js/main.63205766.chunk.js"
+  },
+  {
+    "revision": "a7cae4e8ad70504ff886",
+    "url": "/static/js/2.334254c4.chunk.js"
+  },
+  {
+    "revision": "ebba8b1cdbb6cad1442d",
+    "url": "/static/css/main.3acef3ee.chunk.css"
+  },
+  {
+    "revision": "a7cae4e8ad70504ff886",
+    "url": "/static/css/2.95e96b2c.chunk.css"
+  },
+  {
+    "revision": "9614a37105549f32163cd4ce23835f0a",
+    "url": "/index.html"
+  }
+];
